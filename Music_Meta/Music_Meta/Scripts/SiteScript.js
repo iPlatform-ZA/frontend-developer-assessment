30c7182def67c991776e25bd6fca9530b1c86de6
ArmandJ77 <armandjordaan@live.co.za>
2015-11-20 16:28:52 +0200

    Added search functionality on home page | Created releases object + events | Enable release's artist search

--- Music_Meta/Music_Meta/Scripts/SiteScript.js
+++ Music_Meta/Music_Meta/Scripts/SiteScript.js
@@ -9,26 +9,42 @@ $(document).ready(function (e) {
 });
 
 function ArtistLoad() {
-    $('#txtArtistSearch').val(localStorage.getItem('ArtistSearch'));
-    //$('#txtArtistSearch').val('In Flames');
-    SearchArtist($('#txtArtistSearch').val());
-
+    var term = localStorage.getItem('ArtistSearch');
+    if (term) {
+        SearchArtist(term);
+    }
 }
 
+function ReleaseLoad() {
+    var term = localStorage.getItem('ReleaseSearch');
+    if (term) {
+        SearchRelease(term);
+    }
+}
+
+//#region Global Objects
 var ArtistDetails = [];
+
 var FavoriteArtists = [];
-
-function Artist(id, arid, name) {
+//An object contain the Artist details
+function Artist(id, arid, name,commandtext) {
+    //ID used for iteration and identification
     this.ID = id;
+    //Unique artist id
     this.Arid = arid;
+    //Artis name
     this.Name = name;
+    //variable which stores the artist's search result li
     this.SearchResult = '';
+    //variable which stores the artist's short list li
     this.ShortList = '';
-
+    this.CommandText = commandtext;
+    //A function which populates the searchresult property which my be used for DOM generation
     this.PopulateListItem = function () {
-        this.SearchResult = '<li><h4>' + this.Name + '</h4><button type="button" onclick="AddToShortList(this)" value="' + this.ID + '" class="btn btn-default">Add To Short List</button></li>';
+        this.SearchResult = '<li><h4>' + this.Name + '</h4><button type="button" onclick="AddToShortList(this)" value="' + this.ID + '" class="btn btn-default">' + this.CommandText +'</button></li>';
     };
 
+    //A function which populates the short list property which maybe used for DOM generation
     this.PopulateShortList = function () {
         this.ShortList = '<li><h4>' + this.Name + '</h4>'
             + '<button onclick="DeleteFavorite(this)" class="Delete" type="button" value="' + this.ID + '">'
@@ -41,42 +57,67 @@ function Artist(id, arid, name) {
     };
 };
 
+function Release(id, year, title, label, trackcount, reid, arid) {
+    this.ID = id;
+    //unique artist id
+    this.Arid = arid;
+    //unique release id
+    this.Reid = reid;
+    this.Year = year;
+    this.Title = title;
+    this.Label = label;
+    this.TrackCount = trackcount;
+
+    this.SearchResult = '';
+    this.ReleaseInfo = '';
+
+    this.PopulateSearchResult = function () {
+        this.SearchResult = '<li><h4>' + this.Title + '</h4><button type="button" onclick="AddToShortList(this)" value="' + this.ID + '" class="btn btn-default">Show Releases</button></li>';
+    };
+
+    this.PopulateReleaseInfo = function () {
+
+    };
+}
+//#endregion
+
+
 //#region control events
+//#region Artists Search / Results
 function SearchArtist(term) {
-    if (term) {
-        ShowLoader(1);
-        $('#SearchResults').empty();
-        SearchArtistController(term);
+    if ($.trim(term)) {
+        $('#txtArtistSearch').val('');
+        SearchArtistController(term, $('#SearchResults'),'Add to short list');
         ClearSearch();
+    }
+    else {
+        Notification("Hi, :)", "Artist name may not be blank. Please enter valid artist name.");
     }
 }
 
-$(function () {
-    $('a.page-scroll').bind('click', function (event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+function ArtistHomeSearch(e, term) {
+    //e.preventDefault();
 
-$(function () {
-    $('#aHeaderSearch').click(function (e) {
+    if ($.trim(term)) {
+        localStorage.setItem("ArtistSearch", term);
+        window.location.href = 'Artist.html';
+    }
+    else {
+        Notification("Hi, :)", "Artist name may not be blank. Please enter valid artist name.");
+    }
+}
 
-        e.preventDefault();
+function ReleaseHomeSearch(e, term) {
+    //e.preventDefault();
 
-        var search = $("#txtHeaderSearch").val();
-
-        if ($.trim(search)) {
-            localStorage.setItem("ArtistSearch", $("#txtHeaderSearch").val());
-            window.location.href = 'Artist.html';
-        }
-        else {
-            Notification("Hi, :)", "Artist name may not be blank. Please enter valid artist name.");
-        }
-    })
-});
+    if ($.trim(term)) {
+        localStorage.setItem("ReleaseSearch", term, 'Add to short list');
+        window.location.href = 'Release.html';
+    }
+    else {
+        Notification("Hi, :)", "Artist name may not be blank. Please enter valid artist name.");
+    }
+}
 
 function AddToShortList(e) {
     $('#ArtistShortList').append(ArtistDetails[e.value].ShortList);
@@ -102,9 +143,27 @@ function DeleteFavorite(e) {
 };
 //#endregion
 
+//#region Releases Search / Results
+function SearchRelease(term) {
+    //if ($.trim(term)) {
+    $('#txtSearchRelease').val('');
+    SearchArtistController(term, $('#ReleaseResults'),'Show releases');
+    //SearchReleaseController(term);
+    ClearSearch();
+    //}
+    //else {
+    //    Notification("Hi, :)", "Artist name may not be blank. Please enter valid artist name.");
+    //}
+}
+
+//#endregion
+
 //#region Service Controllers
-function SearchArtistController(artist) {
+function SearchArtistController(artist, targetedul,commandtext) {
     ArtistDetails = [];
+
+    ShowLoader(1);
+    $(targetedul).empty();
 
     var URL = "http://musicbrainz.org/ws/2/artist/?query=artist:" + artist + "&fmt=json";
 
@@ -113,51 +172,134 @@ function SearchArtistController(artist) {
         type: "GET",
         dataType: "json",
         success: function (data) {
-            //var foo = JSON.parse(data);
-
             $.each(data, function (key, val) {
                 for (var i = 0; i < data.artists.length; i++) {
-                    var foo = new Artist(i, data.artists[i]["id"], data.artists[i]["name"]);
+                    var foo = new Artist(i, data.artists[i]["id"], data.artists[i]["name"],commandtext);
                     foo.PopulateListItem();
                     foo.PopulateShortList();
                     ArtistDetails.push(foo);
-                    $('#SearchResults').append(foo.SearchResult);
+                    $(targetedul).append(foo.SearchResult);
                 }
             });
 
             ShowLoader(0);
-
-            //alert(JSON.stringify(ArtistDetails));
-
         },
         error: function (ex) {
             alert("Error could not get data " + ex);
+            ShowLoader(0);
         }
     });
 };
-//#endregion
 
-//#region service requests
-function GetArtistDetails(artist) {
-    var URL = "http://musicbrainz.org/ws/2/artist/?query=artist:" + artist + "&fmt=json";
+function SearchReleaseController(term) {
+    ReleaseDetails = [];
+    ShowLoader(1);
+    $('#ReleaseResults').empty();
+    var URL = "http://musicbrainz.org/ws/2/release/?query=artistname:" + term + "&fmt=json";
 
     $.ajax({
         url: URL,
         type: "GET",
         dataType: "json",
         success: function (data) {
-            var foo = (data);
-            $.each(obj, function (key, val) {
+            $.each(data, function (key, val) {
+                for (var i = 0; i < data.releases.length; i++) {
 
+                    var date = '';
+                    var title = '';
+                    var label = '';
+                    var trackcount = '';
+                    var reid = '';
+                    var arid = '';
+
+                    try {
+                        date = data.releases[i].date;
+                    } catch (e) {
+                        date = 'No Date';
+                    }
+
+                    try {
+                        title = data.releases[i].title;
+                    } catch (e) {
+                        title = 'No Title';
+                    }
+
+                    try {
+                        label = data.releases[i]["label-info"][0].label.name;
+                    } catch (e) {
+                        label = 'No Label';
+                    }
+
+                    try {
+                        trackcount = data.releases[i]["track-count"];
+                    } catch (e) {
+                        trackcount = '0';
+                    }
+
+                    try {
+                        reid = data.releases[i].id;
+                    } catch (e) {
+                        reid = 'No id';
+                    }
+
+                    try {
+                        arid = data.releases[i]["artist-credit"][0].artist.id;
+                    } catch (e) {
+                        arid = 'No id';
+                    }
+
+
+                    var temp = new Release(i, date, title, label, trackcount, reid, arid);
+                    temp.PopulateSearchResult();
+
+                    ReleaseDetails.push(temp);
+                    $('#ReleaseResults').append(temp.SearchResult);
+                    //foo.PopulateShortList();
+
+
+                    //var foo = new Artist(i, data.releases[i]["id"], data.artists[i]["name"]);
+                    //foo.PopulateListItem();
+                    //foo.PopulateShortList();
+                    //ArtistDetails.push(foo);
+                    //$('#SearchResults').append(foo.SearchResult);
+
+
+                }
             });
-            alert(foo);
-            return foo;
+
+            localStorage.setItem('ReleaseDetails', ReleaseDetails);
+            ShowLoader(0);
         },
         error: function (ex) {
+            ShowLoader(0);
             alert("Error could not get data " + ex);
         }
     });
+    //alert('Release count: ' + TempReleaseDetail.length);
 }
+//#endregion
+
+//#region service requests
+//function GetArtistDetails(artist) {
+//    var URL = "http://musicbrainz.org/ws/2/artist/?query=artist:" + artist + "&fmt=json";
+
+//    $.ajax({
+//        url: URL,
+//        type: "GET",
+//        dataType: "json",
+//        success: function (data) {
+//            var foo = (data);
+//            $.each(obj, function (key, val) {
+
+//            });
+//            alert(foo);
+//            return foo;
+//        },
+//        error: function (ex) {
+//            alert("Error could not get data " + ex);
+//        }
+//    });
+//}
 //#endregion
 
 //#region Helper Functions
@@ -197,9 +339,10 @@ function RemoveFavoriteArtist(index) {
 }
 
 function ClearSearch() {
-    $('#txtArtistSearch').val('');
-    $('#txtHeaderSearch').val('');
+    $('#txtHomeReleaseSearch').val('');
+    $('#txtHomeArtistSearch').val('');
     localStorage.setItem('ArtistSearch', '');
+    localStorage.setItem('ReleaseSearch', '');
 }
 //#endregion
 
